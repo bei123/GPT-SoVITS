13c4dcb96ae17017ce16fa6e67bd7e2abaeb3a54
bei123 <102850523+bei123@users.noreply.github.com>
2025-03-23 23:33:47 +0800

    Add files via upload

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-import sys,os
-
+import sys
+import os
 import torch
 
 # 推理用的指定模型
@@ -7,8 +7,8 @@ sovits_path = ""
 gpt_path = ""
 is_half_str = os.environ.get("is_half", "True")
 is_half = True if is_half_str.lower() == 'true' else False
-is_share_str = os.environ.get("is_share","False")
-is_share= True if is_share_str.lower() == 'true' else False
+is_share_str = os.environ.get("is_share", "False")
+is_share = True if is_share_str.lower() == 'true' else False
 
 cnhubert_path = "GPT_SoVITS/pretrained_models/chinese-hubert-base"
 bert_path = "GPT_SoVITS/pretrained_models/chinese-roberta-wwm-ext-large"
@@ -27,21 +27,23 @@ webui_port_uvr5 = 9873
 webui_port_infer_tts = 9872
 webui_port_subfix = 9871
 
-api_port = 9880
+api_port = 6006
 
 if infer_device == "cuda":
     gpu_name = torch.cuda.get_device_name(0)
     if (
             ("16" in gpu_name and "V100" not in gpu_name.upper())
             or "P40" in gpu_name.upper()
-            or "P10" in gpu_name.upper()
+            or "P10" in gpu_name
             or "1060" in gpu_name
             or "1070" in gpu_name
             or "1080" in gpu_name
     ):
-        is_half=False
+        is_half = False
 
-if(infer_device=="cpu"):is_half=False
+if(infer_device == "cpu"):
+    is_half = False
+
 
 class Config:
     def __init__(self):
@@ -64,3 +66,55 @@ class Config:
         self.webui_port_subfix = webui_port_subfix
 
         self.api_port = api_port
+        
+        
+        self.modelToPromptText = {
+            "wendi": "他曾经与我一同聆听风的歌唱，一同弹奏蒲公英的诗篇",
+            "shenglilinghua": "这里有别于神里家的布景，移步之间，处处都有新奇感。",
+            "paimeng": "既然罗莎莉亚说足迹上有元素力，用元素视野应该能很清楚地看到吧。",
+            "leidianjiangjun":"我此番也是受神子之邀，体验一下市井游乐的氛围，和各位并无二致。",
+            "keli":"买东西那天也有一个人帮了开了款式，那个人好像叫",
+            "hutao":"本堂主略施小计，你就败下阵来了，嘿嘿。",
+            "ganyu":"但只要最后落在具体的人身上，那，我可以想办法。",
+            "funingna":"太普通了！哼，这种缺乏特色的料理得不到我的认可！",
+            "bachongshenzi":"在这姑且属于人类的社会里，我也不过凭自己兴趣照做而已",
+            "ailixiya":"我还有好多好多话想对你说，就这样一直继续下去，好吗？",
+            "huahuo":"可聪明的人从一开始就不会入局。你瞧，我是不是更聪明一点？",
+            "bufeiyan":"再把精液涂满我的脸，给我自己做一个精液面膜。",
+            "guodegang":"哲宗皇帝的妹夫，这个九大王赵佶又是他的小舅子。",
+            "hanhong":"谁爱知道，我想什么都没所谓，我所有的一切都是通透的。",
+            "zhoujielun":"对，创作不可能无师自通的，我觉得你应该要这样讲啊。",
+            "jok":"怎么样才能维持一段良好的关系？在维持一段关系的时候，是不是要",
+            "yatuoli":"よしよし、みなも。ほら、見てください。",
+            "yangmi":"就是没有什么说特定的，你只是非常喜欢那个现场的氛围",
+            "sabeining":"并因此，成功破获了一起公安部督办的毒品大案。",
+            "direnjie":"奉天承运，皇帝诏曰，古来圣王治世。",
+            "nigemaiti":"亲爱的朋友们，今天的开幕式现场，我们也荣幸的请到了很多尊贵的来宾，他们是。",
+            
+        }
+
+       
+        self.modelToPromptLanguage = {
+            "wendi": "zh",  
+            "shenglilinghua": "zh",  
+            "paimeng": "zh",  
+            "keli":"zh",
+            "leidianjiangjun":"zh",
+            "hutao":"zh",
+            "ganyu":"zh",
+            "funingna":"zh",
+            "bachongshenzi":"zh",
+            "ailixiya":"zh",
+            "huahuo":"zh",
+            "bufeiyan":"zh",
+            "guodegang":"zh",
+            "zhoujielun":"zh",
+            "jok":"zh",
+            "yatuoli":"ja",
+            "hanhong":"zh",
+            "yangmi":"zh",
+            "sabeining":"zh",
+            "direnjie":"zh",
+            "nigemaiti":"zh",
+            
+        }
